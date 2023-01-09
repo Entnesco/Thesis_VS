1dd4fe5173edb8fae70f587a0318c17cd20afa51
Michal Lerman <grantes208@gmail.com>
2023-01-09 20:15:46 +0100

    test splited files

--- sort_01/main.cpp
+++ sort_01/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <sys/time.h>
-#include <pthread.h>
-#include "data_gen.h"
-#include "time_my.h"
 #include "sort_seq.h"
+#include "sort_paral.h"
+#include "merge_sort_parall.h"
+#include "bubble_sort_paral.h"
+#include "quick_sort_paral.h"
 #include "select_sort_paral.h"
 
 /*
@@ -11,6 +12,11 @@ Runing in terminal:
 g++ -o main.exe main.cpp sort.cpp
 ./main.exe
 */
+
+#define SEED  100
+#define LENGTH 1000
+#define UPPER_LIM 10000
+#define LOWER_LIM  1
 
 using namespace std;
 
@@ -30,22 +36,24 @@ int main(){
 
     srand(SEED);
     struct timeval  start, end;
-    pthread_t threads[NUM_THREADS];
+    int *arr = new int[LENGTH];
 
     // wygenerowanie zbioru danych - tablica z losowymi wartosciami zawierającymi się w wyznaczonym zakresie wartości
-    genereate_data_set();
+    genereate_data_set(arr, LENGTH, LOWER_LIM, UPPER_LIM);
 
     //rozpoczęcie pomiaru czasu
     gettimeofday(&start, NULL);
 
-    selection_sort_parall();
+    selection_sort_parall(arr, LENGTH);
 
     //koniec pomiaru czasu
     gettimeofday(&end, NULL);
 
     //prezentacja wyników w terminalu
-    print_array();
+    print_array(arr, LENGTH);
     print_time(start, end);
+
+    delete [] arr;
 
 
     return 0;
